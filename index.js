53b917ef0b865d871336e61cfa237bb9b634ab2b
wtscott@gmail.com <wtscott@gmail.com>
2018-04-26 15:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+
 function getFirstSelector(sel) {
   return document.querySelector(sel) } 
 
 function nestedTarget() {
-  return document.querySelector('#nested').querySelector('target');
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var parent =  document.getElementById('grand-node');
+  var lis = [];
+  while(parent.querySelector('div') != null){
+    parent = parent.querySelector('div');
+  }
+  return parent;
 }
